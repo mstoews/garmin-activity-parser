4ef75087ee2ba0174a26f1f5708e8fa1ac42d767
mstoews <mstoews@hotmail.com>
2022-09-11 13:35:15 +0900

    trade update ETL

--- party.go
+++ party.go
@@ -5,10 +5,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// insert into trd_amount (trd_recordno, trd_charge_levy_type_p2k, trd_charge_levy_instr_p2k, trd_charge_discount_wil,
-// trd_charge_levy_qty_p2k, trd_charge_levyrate_p2k)
-// values ();
-
 func insertParty(pty *PARTYEXT) {
 	for i := 0; i < len(pty.PARTY); i++ {
 		fmt.Printf("insert into pty_party (pty_partyref, pty_category, pty_longdesc, pty_holiday, pty_country, pty_location, pty_shrtdesc, pty_partynam1, pty_partynam2, pty_partynam3, pty_active, pty_verdat) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');\n",
